f61e62336ce962fce609bdf39c1fddac9a38d585
Cristi Popovici <cristi.popovici@gmail.com>
2016-06-22 20:00:57 +0300

    ongoing work

--- src/main/scala/io/sylphrena/events/EventBus.scala
+++ src/main/scala/io/sylphrena/events/EventBus.scala
@@ -1,17 +1,23 @@
 package io.sylphrena.events
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import java.util.{Timer, TimerTask}
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
 import scala.reflect.ClassTag
 
 import io.sylphrena.events.models.{EventBus, EventHandler}
+import io.sylphrena.execution.CachedExecutionContext
 
 object models {
   type EventHandler[T] = T => Unit
 
   sealed trait EventBus {
     def withAsyncExecution: EventBus
-    def post[T](event: T): Unit
+    def post[T](event: => T)(implicit ec: ExecutionContext = CachedExecutionContext.instance): Unit
+    def postAtInterval[T](interval: FiniteDuration,
+                          initialDelay: FiniteDuration = 0.seconds)(
+        event: => T): Unit
     def subscribe[T](e: EventHandler[T])(implicit c: ClassTag[T]): EventBus
   }
 }
@@ -21,8 +27,11 @@ object EventBus {
 
   private case class EventBusImpl(async: Boolean = false) extends EventBus {
     private[this] val eventHandlerRegistry = new Registry()
+    private[this] val timer = new Timer()
 
-    override def post[T](event: T): Unit =
+    override def post[T](
+        event: => T)(implicit ec: ExecutionContext =
+                       CachedExecutionContext.instance): Unit =
       eventHandlerRegistry.lookUpEventHandler(event).foreach { f =>
         if (async) {
           Future(f(event))
@@ -30,6 +39,14 @@ object EventBus {
           f(event)
         }
       }
+
+    override def postAtInterval[T](interval: FiniteDuration,
+                                   initialDelay: FiniteDuration = 0.seconds)(
+        event: => T): Unit = {
+      timer.scheduleAtFixedRate(new TimerTask {
+        override def run(): Unit = post(event)
+      }, 0, interval.toMillis)
+    }
 
     override def subscribe[T](e: EventHandler[T])(
         implicit c: ClassTag[T]): EventBus = {
---
+++ src/main/scala/io/sylphrena/execution/CachedExecutionContext.scala
@@ -0,0 +1,32 @@
+package io.sylphrena.execution
+
+import java.util.concurrent._
+
+import scala.concurrent.ExecutionContext
+
+/**
+  * 
+  * If the thread pool has not reached the core size, it creates new threads.
+  * If the core size has been reached and there is no idle threads, it queues tasks.
+  * If the core size has been reached, there is no idle threads, and the queue becomes full, it creates new threads (until it reaches the max size).
+  * If the max size has been reached, there is no idle threads, and the queue becomes full, the rejection policy kicks in.
+  * 
+  * Created by dev on 22/06/16.
+  */
+object CachedExecutionContext {
+  val instance = ExecutionContext.fromExecutor(executor)
+
+  private[this] val numProc = Runtime.getRuntime().availableProcessors()
+
+  private[this] val executor = new ThreadPoolExecutor(
+      4, // core size
+      200, // max size
+      10 * 60, // idle timeout
+      TimeUnit.SECONDS,
+      new LinkedBlockingQueue[Runnable]()) // queue with a size
+  executor.allowCoreThreadTimeOut(true)
+}
+
+trait CachedExecutionContext {
+  implicit val defaultExecutionContext = CachedExecutionContext.instance
+}
